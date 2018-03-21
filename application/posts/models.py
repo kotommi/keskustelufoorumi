0b4e204937cdc18d07139416d602d37b809403b6
kotommi <tommi.koivula@helsinki.fi>
2018-03-21 13:21:38 +0200

    added basic post functionality for testing

---
+++ application/posts/models.py
@@ -0,0 +1,14 @@
+from application import db
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    date_created = db.Column(db.DateTime, default=db.func.current_timestamp())
+    date_modified = db.Column(db.DateTime, default=db.func.current_timestamp(),
+    onupdate = db.func.current_timestamp())
+    
+    name = db.Column(db.String(144), nullable=False)
+    content = db.Column(db.String(1000), nullable=False)
+    
+    def __init__(self, name, content):
+        self.name = name
+        self.content = content 
---
+++ application/posts/views.py
@@ -0,0 +1,20 @@
+from application import app, db
+from flask import redirect, render_template, request, url_for
+from application.posts.models import Post
+
+@app.route("/posts/")
+def post_index():
+    return render_template("post/list.html", posts = Post.query.all())
+    
+@app.route("/post/new")
+def post_form():
+    return render_template("post/new.html")
+   
+@app.route("/post/", methods=["POST"])
+def post_create():
+    print(request.form.get("name"))
+    p = Post(request.form.get("name"), request.form.get("content"))
+    db.session().add(p)
+    db.session().commit()
+    return redirect(url_for("post_index"))
+
