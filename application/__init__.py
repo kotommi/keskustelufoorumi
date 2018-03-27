7f95e4679b76b5f64c13ef0a04a389791aec7c90
kotommi <tommi.koivula@helsinki.fi>
2018-03-27 13:31:05 +0300

    __very__ basic login functionality

--- application/__init__.py
+++ application/__init__.py
@@ -1,17 +1,18 @@
 import flask
-import flask_sqlalchemy
-
+from flask_sqlalchemy import SQLAlchemy
 
 app = flask.Flask(__name__)
-db = flask_sqlalchemy.SQLAlchemy(app)
-
-
-from application import views
-from application.posts import views
-
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///posts.db"
 app.config["SQLALCHEMY_ECHO"] = True
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
+db = SQLAlchemy(app)
+
+from application import views
+from application.posts import views
+from application.posts import models
+
+from application.auth import models
+from application.auth import views
 
 db.create_all()
