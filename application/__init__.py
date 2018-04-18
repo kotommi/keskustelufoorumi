34c1ed8ffa5a141b87cd44f4ad099220853ee0a4
kotommi <tommi.koivula@helsinki.fi>
2018-04-18 11:53:26 +0300

    refactoring

--- application/__init__.py
+++ application/__init__.py
@@ -20,22 +20,17 @@ from application.auth import models
 from application.posts import models
 from application.threads.models import Thread
 from application.threads import models
+from application.auth.models import User
+
+# create tables if not present
+db.create_all()
 
 # login
-from application.auth.models import User
 from os import urandom
 
 app.config["SECRET_KEY"] = urandom(32)
 
-db.create_all()
-
-from application.auth import views
-from application.category import views
-from application.threads import views
-from application import views
-from application.posts import views
-
-from flask_login import LoginManager
+from flask_login import LoginManager, current_user
 
 login_manager = LoginManager()
 login_manager.init_app(app)
@@ -43,7 +38,45 @@ login_manager.init_app(app)
 login_manager.login_view = "auth_login"
 login_manager.login_message = "Please login to use this functionality."
 
+# decorator for roles in login_required
+from functools import wraps
+
+
+def login_required(role="ANY"):
+    def wrapper(fn):
+        @wraps(fn)
+        def decorated_view(*args, **kwargs):
+            if not current_user.is_authenticated():
+                return login_manager.unauthorized()
+
+            unauthorized = False
+
+            if role != "ANY":
+                unauthorized = True
+
+                for user_role in current_user.roles():
+                    if user_role == role:
+                        unauthorized = False
+                        break
+
+            if unauthorized:
+                return login_manager.unauthorized()
+
+            return fn(*args, **kwargs)
+
+        return decorated_view
+
+    return wrapper
+
 
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(user_id)
+
+
+# load views
+from application.auth import views
+from application.category import views
+from application.threads import views
+from application import views
+from application.posts import views
--- application/auth/models.py
+++ application/auth/models.py
@@ -29,6 +29,9 @@ class User(Base):
     def is_authenticated(self):
         return True
 
+    def roles(self):
+        return ["ADMIN"]
+
     @staticmethod
     def find_usernames():
         statement = text(
