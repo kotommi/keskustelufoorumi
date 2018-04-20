89d78372e9eec0cc53ad161ff47be3cbabdd3866
kotommi <tommi.koivula@helsinki.fi>
2018-04-20 22:48:51 +0300

    psql fix

--- application/models.py
+++ application/models.py
@@ -5,6 +5,5 @@ class Base(db.Model):
     __abstract__ = True
 
     id = db.Column(db.Integer, primary_key=True)
-    date_created = db.Column(db.Integer, default=db.func.current_timestamp())
-    date_modified = db.Column(db.Integer, default=db.func.current_timestamp(), onupdate=db.func.current_timestamp())
-
+    date_created = db.Column(db.DateTime, default=db.func.current_timestamp())
+    date_modified = db.Column(db.DateTime, default=db.func.current_timestamp(), onupdate=db.func.current_timestamp())
